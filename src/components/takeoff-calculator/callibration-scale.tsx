6bbf26a4f4cfe9605195d3b55e56331568fc0cac
Ramish Amir <ramishamir2750@gmail.com>
2025-09-22 12:44:47 -0400

    Implemented viewport-dimentions based scaling

--- src/components/takeoff-calculator/callibration-scale.tsx
+++ src/components/takeoff-calculator/callibration-scale.tsx
@@ -11,20 +11,29 @@ import {
 import {
   DEFAULT_CALLIBRATION_VALUE,
   DrawingCalibrations,
+  getDrawingCallibrations,
 } from "@/lib/drawingCallibrations";
 
 type DrawingCallibrationScaleProps = {
   setScaleFactor: (newFactorValue: number) => void;
+  viewportDimensions: { width: number; height: number };
 };
 
 export const DrawingCallibrationScale = ({
   setScaleFactor,
+  viewportDimensions,
 }: DrawingCallibrationScaleProps) => {
   return (
     <>
       <Select
         defaultValue={DEFAULT_CALLIBRATION_VALUE}
-        onValueChange={(value) => setScaleFactor(DrawingCalibrations[value])}
+        onValueChange={(value: keyof typeof DrawingCalibrations) => {
+          const callibratedValue = getDrawingCallibrations(
+            value,
+            viewportDimensions
+          );
+          return setScaleFactor(callibratedValue);
+        }}
       >
         <SelectTrigger className="w-[80px] min-w-max flex h-9 items-center justify-between whitespace-nowrap rounded-md border border-primary text-primary bg-background px-3 py-2 text-sm shadow-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1">
           <Scaling strokeWidth={2} size={16} className="mr-2" /> <SelectValue />
